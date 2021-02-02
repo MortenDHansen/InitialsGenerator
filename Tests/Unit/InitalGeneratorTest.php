770f98fcace4f24ee3a055a68a5a0ba18d7a0dad
Morten D. Hansen <morten@visia.dk>
2021-02-02 08:50:31 +0100

    Add code

--- Tests/Unit/InitalGeneratorTest.php
+++ Tests/Unit/InitalGeneratorTest.php
@@ -3,16 +3,11 @@
 
 namespace Unit;
 
-
 use InitialsGenerator\Generator;
 use PHPUnit\Framework\TestCase;
 
 class InitalGeneratorTest extends TestCase
 {
-
-    public $namesDk = [
-
-    ];
 
     /**
      * @test
@@ -20,7 +15,99 @@ class InitalGeneratorTest extends TestCase
     public function canCallGenerator()
     {
         $generator = new Generator();
-        $this->assertTrue($generator->generate('Peter Pan'));
+        $this->assertTrue(is_string($generator->generate('Peter Pan')));
+    }
+
+    /**
+     * @test
+     */
+    public function canSeparateNameParts()
+    {
+        $generator = new Generator();
+        $generator->extractNameParts('Bruno Seirup-Thinnesen');
+        $this->assertEquals(3, count($generator->nameParts));
+    }
+
+    public function runAllNamesInList()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function cleanName()
+    {
+        $generator = new Generator();
+
+        $cleanedName = $generator->cleanupString('Elżbieta');
+        $this->assertEquals('elzbieta', $cleanedName);
+
+        $cleanedName = $generator->cleanupString('Søren');
+        $this->assertEquals('soren', $cleanedName);
+
+        $cleanedName = $generator->cleanupString('Żaneta');
+        $this->assertEquals('zaneta', $cleanedName);
+    }
+
+    /**
+     * @test
+     */
+    public function generatesInitials()
+    {
+        $generator = new Generator();
+        $initials = $generator->generate('Peter Pan');
+        $this->assertTrue(strlen($initials) == 3);
+        $this->assertEquals('ppa', $initials);
+
+        $initials = $generator->generate('Bruno Seirup-Thinnesen');
+        $this->assertTrue(strlen($initials) == 3);
+        $this->assertEquals('bst', $initials);
+    }
+
+    /**
+     * @test
+     */
+    public function suggestDifferentCombinationIfInitialAlreadyUsedForDoubleNames()
+    {
+        // If first priority is taken, try using first two letters of firstname and first letter of last name
+        $generator = new Generator();
+        $initials = $generator->generate('Peter Pan', ['ppa']);
+        $this->assertTrue(strlen($initials) == 3);
+        $this->assertEquals('pep', $initials);
+    }
+
+    /**
+     * @test
+     */
+    public function suggestAnotherComboIfthereAreMultipleNamesAndFirstComboWasTaken()
+    {
+        $generator = new Generator();
+        $initials = $generator->generate('Peter Pingo Pan', ['ppa', 'ppe']);
+        $this->assertTrue(strlen($initials) == 3);
+        $this->assertEquals('ppp', $initials);
+    }
+
+    /**
+     * @test
+     */
+    public function ifThereAreThreeNamesTryWithFirstTwoLettersOfMiddle()
+    {
+        $generator = new Generator();
+        $initials = $generator->generate('Peter Pingo Pan', ['ppa', 'ppe', 'ppp', 'pep']);
+        $this->assertTrue(strlen($initials) == 3);
+        $this->assertEquals('ppi', $initials);
+    }
+
+    /**
+     * @test
+     */
+    public function tryUsingFirstAndLastLetterOfLastName()
+    {
+        $generator = new Generator();
+        $initials = $generator->generate('Peter Pingo Pan', ['ppa', 'ppe', 'ppp', 'pep', 'ppi']);
+        $this->assertTrue(strlen($initials) == 3);
+        $this->assertEquals('ppn', $initials);
     }
 
 }
--- src/Generator.php
+++ src/Generator.php
@@ -4,6 +4,8 @@
 namespace InitialsGenerator;
 
 
+use Illuminate\Support\Str;
+
 class Generator
 {
 
@@ -12,13 +14,79 @@ class Generator
      */
     private $takenInitials = [];
 
+    private $letters = [];
+    /**
+     * @var array|string[]
+     */
+    public $nameParts;
+    /**
+     * @var array|mixed|string[]
+     */
+    public $lastName;
+    /**
+     * @var mixed|string
+     */
+    public $firstName;
+
     public function __construct()
     {
     }
 
-    public function generate(string $name): string
+    public function extractNameParts($name)
     {
-        return '';
+        // split name at -
+        $name = str_replace('-', ' ', $name);
+
+        // split name to array of parts
+        $nameParts = explode(' ', $name);
+        // Make sure there are no empty parts (when people add too many spaces by mistake)
+        $nameParts = array_filter($nameParts);
+
+        // Replace crazy characters
+        $this->nameParts = array_map(function($namePart) {
+            return $this->cleanupString($namePart);
+        }, $nameParts);
+
+        // specifically extract first and last name
+        $this->firstName = $this->nameParts[0];
+        $this->lastName = last($this->nameParts);
+    }
+
+    public function cleanupString($name)
+    {
+        return Str::slug($name);
+    }
+
+    public function generate(string $name, $cantBeIn = [], $priority = 0): string
+    {
+        $this->extractNameParts($name);
+
+        // We have a problem, lets prioritize solutions:
+        switch ($priority) {
+            case 0: // Try using first two letters from first name, and first from last
+                $this->primarySolution();
+                break;
+            case 1:
+                $this->secondarySolution();
+                break;
+            case 2:
+                $this->thirdSolution();
+                break;
+            case 3:
+                $this->fourthSolution();
+                break;
+            default:
+                return 'err';
+        }
+
+        // check if valid and return.
+        if(!in_array(implode($this->letters), $cantBeIn)) {
+            return implode('', $this->letters);
+        }
+
+        // Up the priority and call yourself to try something different.
+        $priority++;
+        return self::generate($name, $cantBeIn, $priority);
     }
 
     public function batchGenerate(array $names): array
@@ -35,6 +103,59 @@ class Generator
     {
         $this->takenInitials = [];
         return $this;
+    }
+
+    public function solutionPriority()
+    {
+    }
+
+    /**
+     * if name has > 2 parts, use first letter from first two and first from last.
+     * Else we use first letter of first name and two first letters of last name
+     * @param array $nameParts
+     */
+    private function primarySolution()
+    {
+        // Select 3 letters
+        $this->letters = array_fill(0,3,'');
+
+        // Use first letter from first namepart
+        $this->letters[0] = substr($this->firstName, 0, 1);
+
+        // If name has more than two parts, use first letter from first, second and last part
+        if (count($this->nameParts) > 2) {
+            $this->letters[1] = substr($this->nameParts[1], 0, 1);
+            $this->letters[2] = substr($this->lastName, 0, 1);
+        }
+
+        // If we are still missing a part, we take first and second letter from last part.
+        if(empty($this->letters[1])) {
+            $this->letters[1] = substr($this->lastName, 0, 1);
+            $this->letters[2] = substr($this->lastName, 1, 1);
+        }
+    }
+
+    private function secondarySolution()
+    {
+        // Lets try the same as the first solution, but using the first two letters from first name
+        $this->letters[1] = substr($this->firstName, 1, 1);
+        $this->letters[2] = substr($this->lastName, 0, 1);
+    }
+
+    private function thirdSolution()
+    {
+        // if there is a middlename, try using the first two letters from that
+        if (count($this->nameParts) > 2) {
+            $this->letters[1] = substr($this->nameParts[1], 0, 1);
+            $this->letters[2] = substr($this->nameParts[1], 1, 1);
+        }
+    }
+
+    private function fourthSolution()
+    {
+        // try using the first last letter of last name
+        $this->letters[1] = substr($this->lastName, 0, 1);
+        $this->letters[2] = substr($this->lastName, -1, 1);
     }
 
 }
